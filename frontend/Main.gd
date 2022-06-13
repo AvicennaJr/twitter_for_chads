421868a6d9f913c32cd5a8eef74e4f85924bc268
Avicenna <87450618+AvicennaJr@users.noreply.github.com>
2022-06-13 22:48:53 +0300

    Create Main.gd

---
+++ frontend/Main.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var tweet_content
+
+func _ready():
+	$LineEdit.focus_mode = true
+
+func _on_Tweet_pressed():
+	
+	var tweet = {
+		"tweet": tweet_content
+	}
+	var query = JSON.print(tweet)
+	var headers = ["Content-Type: application/json"]
+	
+	$HTTPRequest.request("http://localhost:8000/tweet", headers, false, HTTPClient.METHOD_POST, query)
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
