f298082228b9189e1fea89416355459090608da7
Avicenna <87450618+AvicennaJr@users.noreply.github.com>
2022-06-13 22:44:47 +0300

    Create tweet_main.py

---
+++ backend/tweet_main.py
@@ -0,0 +1,15 @@
+import tweepy
+from keys import keys
+
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+
+def send_a_tweet(tweet):
+    api.update_status (status = tweet, auto_populate_reply_metadata=True)
+    return {"message": "tweet sent"}
